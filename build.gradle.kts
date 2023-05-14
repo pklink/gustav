4605a1b30ce6b1141af194fcb87a8e4e610555a5
Pierre Klink <pierre+dev@einself.net>
2023-05-14 22:45:51 +0200

    chore: added fatJar task

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,18 @@ kotlin {
 }
 
 application {
-    mainClass.set("MainKt")
+    mainClass.set("net.einself.gustav.MainKt")
+}
+
+tasks {
+    register<Jar>("fatJar") {
+        dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources"))
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        manifest { attributes(mapOf("Main-Class" to application.mainClass)) }
+        val sourcesMain = sourceSets.main.get()
+        val contents = configurations.runtimeClasspath.get()
+            .map { if (it.isDirectory) it else zipTree(it) } + sourcesMain.output
+        from(contents)
+    }
 }
 
